85d01b02047065fc554f4412ee61b5165c670aef
Subhash Shrestha <subhashshrestha3@gmail.com>
2021-03-02 14:40:57 +1030

    renamed table field and database, and created seeds.sql as test data file

--- schema.sql
+++ schema.sql
@@ -5,27 +5,27 @@ CREATE DATABASE `employee_tracker`;
 USE `employee_tracker`;
 
 CREATE TABLE `department` (
-	`name` varchar(30) NOT NULL,
-	`id` int NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(30) NOT NULL,
+	`id` INT NOT NULL AUTO_INCREMENT,
 	PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `employee` (
-	`id` int NOT NULL AUTO_INCREMENT,
-	`first_name` varchar(30) NOT NULL,
-	`last_name` varchar(30) NOT NULL,
-	`role_id` int NOT NULL,
-	`manager_id` int,
-	PRIMARY KEY (`id`),
-    FOREIGN KEY (`role_id`) REFERENCES `role`(`id`),
-    FOREIGN KEY (`manager_id`) REFERENCES `employee`(`id`)
-);
-
 CREATE TABLE `role` (
-	`title` varchar(30) NOT NULL,
+	`title` VARCHAR(30) NOT NULL,
 	`salary` DECIMAL NOT NULL,
 	`department_id` int NOT NULL,
 	`id` int NOT NULL AUTO_INCREMENT,
 	PRIMARY KEY (`id`),
     FOREIGN KEY (`department_id`) REFERENCES `department`(`id`)
+);
+
+CREATE TABLE `employee` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`first_name` VARCHAR(30) NOT NULL,
+	`last_name` VARCHAR(30) NOT NULL,
+	`role_id` INT NOT NULL,
+	`manager_id` INT NULL,
+	PRIMARY KEY (`id`),
+    FOREIGN KEY (`role_id`) REFERENCES `role`(`id`),
+    FOREIGN KEY (`manager_id`) REFERENCES `employee`(`id`)
 );
---
+++ seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Dan", "Drinkwater",2,NULL), ("Manjila", "Bajimaya",1,1);
+
+INSERT INTO role (title,salary, department_id) 
+VALUES (`manager`, 150000, 3);
+
+INSERT INTO department (name) 
+VALUES (`Finance`),(`Research`),(`Human Resource`),(`Sales`),;
