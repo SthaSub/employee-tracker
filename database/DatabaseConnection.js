37ebea48f4a1ab912aecb57136be22682c868c71
Subhash Shrestha <subhashshrestha3@gmail.com>
2021-03-09 18:26:57 +1030

    created seed and schema sql

---
+++ database/DatabaseConnection.js
@@ -0,0 +1,34 @@
+const mysql = require("mysql");
+require("dotenv").config();
+
+const config = {
+    host:process.env.DB_HOST,
+    database:process.env.DB_NAME,
+    user:process.env.DB_USER,
+    password:process.env.DB_PASS,
+    port:process.env.DB_PORT
+}
+
+module.exports = class Database{
+    constructor(){
+        this.connection = mysql.createConnection(config);
+        this.connection.connect((err)=>{
+            if (err) throw err;
+            else console.log("connect");
+        });
+    }
+
+    dbQuery(statement, parameter){
+        return new Promise((resolve,reject)=>{
+            this.connection.query(statement, parameter, (err, res)=>{
+                if(err) reject(err);
+                else resolve(res);
+            });
+        })
+    }
+    dbClose(){
+        return new Promise((resolve, reject)=>{
+            resolve(this.connection.end());
+        });
+    }
+}
---
+++ database/seeds.sql
@@ -0,0 +1,26 @@
+-- Employee Dummy Data
+INSERT INTO `` (`id`,`first_name`,`last_name`,`role_id`,`manager_id`) VALUES (1,'John','Hunt',8,NULL);
+INSERT INTO `` (`id`,`first_name`,`last_name`,`role_id`,`manager_id`) VALUES (2,'Manjila','Bajimaya',3,NULL);
+INSERT INTO `` (`id`,`first_name`,`last_name`,`role_id`,`manager_id`) VALUES (3,'Jakee','Sherpa',6,NULL);
+INSERT INTO `` (`id`,`first_name`,`last_name`,`role_id`,`manager_id`) VALUES (4,'Jo','Rodriquez',1,NULL);
+INSERT INTO `` (`id`,`first_name`,`last_name`,`role_id`,`manager_id`) VALUES (51,'Subhash','Shrestha',3,2);
+INSERT INTO `` (`id`,`first_name`,`last_name`,`role_id`,`manager_id`) VALUES (52,'Dan','Drinkwater',9,1);
+INSERT INTO `` (`id`,`first_name`,`last_name`,`role_id`,`manager_id`) VALUES (53,'Yotasi','Nakamura',7,3);
+INSERT INTO `` (`id`,`first_name`,`last_name`,`role_id`,`manager_id`) VALUES (54,'Maria','Inler',2,4);
+
+-- Department Dummy Data
+INSERT INTO `` (`id`,`name`) VALUES (1,'Finance');
+INSERT INTO `` (`id`,`name`) VALUES (2,'Research');
+INSERT INTO `` (`id`,`name`) VALUES (3,'IT');
+INSERT INTO `` (`id`,`name`) VALUES (4,'Sales');
+
+-- Role Dummy Data
+INSERT INTO `` (`id`,`title`,`salary`,`department_id`) VALUES (1,'Sales Manager',100000,4);
+INSERT INTO `` (`id`,`title`,`salary`,`department_id`) VALUES (2,'Sales Person',50000,4);
+INSERT INTO `` (`id`,`title`,`salary`,`department_id`) VALUES (3,'Lead Researcher',250000,2);
+INSERT INTO `` (`id`,`title`,`salary`,`department_id`) VALUES (4,'Assistant Researcher',90000,2);
+INSERT INTO `` (`id`,`title`,`salary`,`department_id`) VALUES (5,'Junior Researcher',30000,2);
+INSERT INTO `` (`id`,`title`,`salary`,`department_id`) VALUES (6,'IT Officer',120000,3);
+INSERT INTO `` (`id`,`title`,`salary`,`department_id`) VALUES (7,'IT Assistant',80000,3);
+INSERT INTO `` (`id`,`title`,`salary`,`department_id`) VALUES (8,'Finance Manager',115000,1);
+INSERT INTO `` (`id`,`title`,`salary`,`department_id`) VALUES (9,'Accountant',85000,1);
